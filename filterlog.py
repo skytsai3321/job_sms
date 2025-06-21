47a138d304ece23ec093276f439d6575841327d0
Leo Tsai <leo52668@gmail.com>
2025-06-21 21:15:47 +0800

    第一次整合

--- filterlog.py
+++ filterlog.py
@@ -5,10 +5,10 @@ def filter_log(file_path):
     jobs = []
     with open(file_path, "r", encoding="utf-8") as log:
         for line in log.readlines():
-            # print(line)
             job = re.search(r"JOB: \w+", line)
             if job is not None:
                 jobs.append(job)
+
     return jobs
 
 
--- watchlog.py
+++ watchlog.py
@@ -1,10 +1,12 @@
 import asyncio
+import filterlog
 from watchfiles import awatch
 
 
 async def watch_log(file_path):
     async for changes in awatch(file_path):
         print(changes)
+        filterlog.filter_log(file_path)
 
 
 if __name__ == "__main__":
